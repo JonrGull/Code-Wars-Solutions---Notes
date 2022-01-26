2faa93e26b4b7cf33ed2192ca364bdcfc01d799e
Jon <jonrgull@gmail.com>
2022-01-26 15:44:00 +0900

    6 Kyu

--- 6 Kyu/Sum of Digits Digital Root.js
+++ 6 Kyu/Sum of Digits Digital Root.js
@@ -18,6 +18,13 @@ function test(actual, expected) {
 // Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced.
 // The input will be a non-negative integer.
 
+/* 
+16  -->  1 + 6 = 7
+942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2 
+*/
+
 //Input: number
 //Output: SINGLE digit number
 
@@ -29,11 +36,11 @@ function digital_root(num) {
   let cache; // need to hold on to this value to pass again next recursion call
 
   if (num.toString().length !== 1) {
-    cache = num
+    (cache = num
       .toString()
       .split("")
-      .map((n) => Number(n))
-      .reduce((acc, curr) => acc + curr);
+      .reduce((acc, curr) => +acc + +curr)), // Convert back to Number
+      0;
   }
   return digital_root(cache);
 }
