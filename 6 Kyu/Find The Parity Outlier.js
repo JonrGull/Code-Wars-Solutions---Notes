f28056b631d66162222a0bac98e549a9e3e99869
Jon <jonrgull@gmail.com>
2022-02-01 00:09:39 +0900

    6 Kyu

--- 6 Kyu/Find The Parity Outlier.js
+++ 6 Kyu/Find The Parity Outlier.js
@@ -16,8 +16,9 @@ function test(actual, expected) {
 
 /* 
 You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
-The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
-Write a method that takes the array as an argument and returns this "outlier" N.
+The array is either entirely comprised of odd integers or entirely 
+comprised of even integers except for a single integer N. Write a method that takes the array as an argument 
+and returns this "outlier" N.
 
 Examples
 [2, 4, 0, 100, 4, 11, 2602, 36]
@@ -47,3 +48,10 @@ test(findOutlier([1, 2, 3]), 2);
 test(findOutlier([2, 6, 8, 10, 3]), 3);
 test(findOutlier([0, 0, 3, 0, 0]), 3);
 test(findOutlier([1, 1, 0, 1, 1]), 0);
+
+// This is the most simple solution with methods
+/* function findOutlier(array){
+  const even = array.filter(num => num % 2 === 0);
+  const odd  = array.filter(num => num % 2 !== 0);
+  return even.length === 1 ? even[0] : odd[0];
+} */
