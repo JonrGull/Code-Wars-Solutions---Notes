d2f6c4879f5f7d9078f213443833310c5f56f914
Jon <jonrgull@gmail.com>
2022-01-31 23:56:51 +0900

    Kyu 6

--- 6 Kyu/Find The Parity Outlier.js
+++ 6 Kyu/Find The Parity Outlier.js
@@ -11,7 +11,6 @@ function test(actual, expected) {
     console.error("Test FAILED. Keep trying!");
     console.log("    actual: ", actual);
     console.log("  expected: ", expected);
-    console.trace();
   }
 }
 
@@ -28,11 +27,21 @@ Should return: 160 (the only even number)
 
 //Input: array
 //output: integer that is different (even or odd) from the rest of the array
-function findOutlier(integers) {
 
+//This is ugly, but it works without methods
+function findOutlier(array) {
+  let evenArray = [];
+  let oddArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) evenArray.push(array[i]);
+    else oddArray.push(array[i]);
+  }
+  if (evenArray.length === 1) return +evenArray;
+  if (oddArray.length === 1) return +oddArray;
 }
 test(findOutlier([0, 1, 2]), 1);
 test(findOutlier([1, 2, 3]), 2);
 test(findOutlier([2, 6, 8, 10, 3]), 3);
-// test(findOutlier([0, 0, 3, 0, 0]), 3);
-// test(findOutlier([1, 1, 0, 1, 1]), 0);
+test(findOutlier([0, 0, 3, 0, 0]), 3);
+test(findOutlier([1, 1, 0, 1, 1]), 0);
